f7b7d1e51ed4ea684496954e5fefb62a2a462100
Sheetal07Jun <115865296+Sheetal07Jun@users.noreply.github.com>
2022-11-08 14:50:00 +0530

    Add files via upload
    using for loop we can make a table of any number

---
+++ table.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class table {
+
+	public static void main(String[] args) {
+	      int num;
+	      System.out.println("enter any number");
+	      Scanner a=new Scanner(System.in);
+	      num=a.nextInt();
+	      for(int i=1;i<=10;i++)
+	      {
+	    	  System.out.println(num*i);
+	      }
+
+	}
+
+}
